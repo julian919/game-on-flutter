c9f7bb231df8b5425daee115dfc271c02234fb0d
Julian Lieow Ter Foo <>
2022-06-20 11:28:25 +0800

    enabled android internet permission and removed unused codes in api function

--- gameon/lib/functions/api.dart
+++ gameon/lib/functions/api.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -6,34 +5,13 @@ class Network {
   final String? _url = dotenv.env['API_URL'];
   final String? _key = '&key=' + dotenv.env['API_KEY']!;
 
-  // _getToken() async {
-    // SharedPreferences sharedPref = await SharedPreferences.getInstance();
-  //   token = sharedPref.getString("token") ?? "";
-  // }
-
-  postData(data, apiUrl) async {
-    var fullUrl = _url! + apiUrl;
-    // await _getToken();
-    return await http.post(Uri.parse(fullUrl),
-        body: jsonEncode(data), headers: _setHeaders());
-  }
-
-  putData(data, apiUrl) async {
-    var fullUrl = _url! + apiUrl;
-    // await _getToken();
-    return await http.put(Uri.parse(fullUrl),
-        body: jsonEncode(data), headers: _setHeaders());
-  }
-
   getData(apiUrl) async {
     var fullUrl = _url! + apiUrl + _key!;
-    // await _getToken();
     return await http.get(Uri.parse(fullUrl), headers: _setHeaders());
   }
 
   _setHeaders() => {
         'Content-type': 'application/json',
         'Accept': 'application/json',
-        // 'Authorization': 'Bearer $token'
       };
 }
